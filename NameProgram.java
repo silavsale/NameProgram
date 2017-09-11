152ec45bc6391888692be0fb702d50367ccc47b2
Rsofku <silavsale@gmail.com>
2017-09-10 23:30:57 -0400

    First commit, java file

---
+++ NameProgram.java
@@ -0,0 +1,11 @@
+
+// Program print the text line
+
+public class NameProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("  My Name is Sergiy !!! \n I am student at Vanier College !");
+	}
+}
+
