cc086f23c6fd527a37ea4e5e4d882bd645c9ca54
Rsofku <silavsale@gmail.com>
2017-09-11 01:43:22 -0400

    Updated: added boolean variable and void method.

--- NameProgram.java
+++ NameProgram.java
@@ -3,9 +3,29 @@
 
 public class NameProgram
 {
+	 boolean graduated = false;
+	
+		public static void printName(boolean graduated){
+			
+			if (graduated == true) {
+				System.out.println("And i drive red Ferrari");
+			}
+				
+			else 
+			{
+				System.out.println("I drive a bicycle!");
+			}
+			
+		}
+	
 	public static void main(String[] args)
 	{
-		System.out.println("  My Name is Sergiy !!! \n I am student at Vanier College !");
+		System.out.println("My Name is Sergiy !!! \nI am student at Vanier College !");
+			
+		printName(false);
+		
 	}
+	
+
 }
 
